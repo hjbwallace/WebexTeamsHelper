c6450abcdc54117ccf8076fc2cedf8082d02aa88
Harrison <hjbwallace@gmail.com>
2019-08-05 20:34:02 +0800

    Adding complex message payload tests

---
+++ src/WebexTeamsHelper.Tests/WebexTeamsMessageBuilderPayloadComplexTests.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+
+namespace WebexTeamsHelper.Tests
+{
+    public class WebexTeamsMessageBuilderComplexPayloadTests : PayloadTestBase
+    {
+        [Fact]
+        public void AddLineWithMultipleFormattingOptions()
+        {
+            var boldText = WebexTeamsFormatting.Bold("Bold");
+            var italicText = WebexTeamsFormatting.Italic("Italic");
+            var inlineText = WebexTeamsFormatting.Inline("Inline");
+
+            var expected = "Line with **Bold**, *Italic* and `Inline`.";
+
+            AssertPayload(x => x.
+                AddLine($"Line with {boldText}, {italicText} and {inlineText}."),
+                expected);
+        }
+    }
+}
